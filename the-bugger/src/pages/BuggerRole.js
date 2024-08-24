3c5ec1a28a4422c01facf242d1ce3b59c23797f1
yushen-qiu <daniel.qys@gmail.com>
2024-08-24 21:46:51 +1000

    created role pages for bugger and coder

---
+++ the-bugger/src/pages/BuggerRole.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BuggerRole() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-neutral-900">
+      <h1 className="text-red-700 text-9xl">Bugger</h1>
+    </div> 
+  );
+}
+
+export default BuggerRole;
---
+++ the-bugger/src/pages/CoderRole.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function CoderRole() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-neutral-900">
+      <h1 className="text-sky-500 text-9xl">Coder</h1>
+    </div> 
+  );
+}
+
+export default CoderRole;
