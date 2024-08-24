e8dd9232fb92f8c279fc0fa15290b70401871d88
Alan Xie <alanxie123@gmail.com>
2024-08-24 21:36:32 +1000

    idk

--- the-bugger/src/index.js
+++ the-bugger/src/index.js
@@ -1,8 +1,9 @@
 // src/index.js
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- the-bugger/tailwind.config.js
+++ the-bugger/tailwind.config.js
@@ -1,9 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+// tailwind.config.js
 module.exports = {
-  content: ["./src/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
+
 
