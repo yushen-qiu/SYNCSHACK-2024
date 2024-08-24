8f157da72f2e0140a18ed356d057122cf71d39f6
Michael <mgoz6544@uni.sydney.edu.au>
2024-08-24 19:52:40 +1000

    initial

--- the-bugger/src/App.js
+++ the-bugger/src/App.js
@@ -1,22 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Lobby from "./pages/Lobby";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="">
+      <header className="bg-slate-400">
+        <p className='text-red-500'>Test</p>
       </header>
     </div>
   );
