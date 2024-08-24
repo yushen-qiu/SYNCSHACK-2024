1055b4a598be029f2ce567f6b49057daa74b89b6
gnittingCodes <shing.ng9495@gmail.com>
2024-08-24 21:43:07 +1000

    added join link to lobby

--- the-bugger/src/App.js
+++ the-bugger/src/App.js
@@ -3,17 +3,8 @@ import logo from './logo.svg';
 import './App.css';
 import Lobby from "./pages/Lobby";
 import Landing from "./pages/Landing";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Router } from "react-router-dom";
 
-// function App() {
-//   return (
-//     <div className="">
-//       <header className="bg-slate-400">
-//         <p className='text-red-500'>Test</p>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
   return React.createElement(
