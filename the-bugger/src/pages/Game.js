18eb2e14d2ba989e52b4ba519782498b1b1b28cf
Alan Xie <alanxie123@gmail.com>
2024-08-24 23:30:27 +1000

    made title  and instruction box bigger

--- the-bugger/src/pages/Game.js
+++ the-bugger/src/pages/Game.js
@@ -6,11 +6,11 @@ function GamePage() {
     <div className="min-h-screen bg-seashell p-4">
     {/* Header */}
     <div className="flex justify-between items-center px-10 py-4">
-      <div className="text-2xl font-bold text-center">
+      <div className="text-4xl font-bold text-center w-fit">
         NAME PLACEHOLDER
       </div>
 
-      <div id='instructions' className="bg-rose-taupe text-white text-lg px-20 py-3 rounded-lg mr-4">
+      <div id='instructions' className="bg-rose-taupe text-white text-lg text-center w-1/2 py-3 rounded-lg mr-4">
             Code a calculator
       </div>
 
@@ -26,9 +26,9 @@ function GamePage() {
     </div>
 
     <div class="flex justify-center items-start space-x-4 p-8 bg-seashell h-[calc(100vh-10rem)]">
-    <div class="w-2/3 bg-dessert-sand rounded-lg shadow-md h-[calc(100vh-10rem)]">Code editor</div>
+    <div class="w-3/4 bg-dessert-sand rounded-lg shadow-md h-[calc(100vh-10rem)]">Code editor</div>
 
-    <div class="w-1/3 bg-dessert-sand rounded-lg shadow-md h-[calc(100vh-10rem)]">Chat</div>
+    <div class="w-1/4 bg-dessert-sand rounded-lg shadow-md h-[calc(100vh-10rem)]">Chat</div>
     </div>
 
     </div>
