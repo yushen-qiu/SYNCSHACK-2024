eeae6949d4bbb91485017dde3740d55e6745c54a
Alan Xie <alanxie123@gmail.com>
2024-08-24 23:22:38 +1000

    base done

--- the-bugger/tailwind.config.js
+++ the-bugger/tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors')
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'wenge': '#584b53',
+        'rose-taupe': '#9d5c63',
+        'lavender': '#d6e3f8',
+        'seashell': '#fef5ef',
+        'dessert-sand': '#e4bb97',
+      },
+    },
   },
   plugins: [],
 }
