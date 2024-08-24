6109290fdf3ed431195062bbdffe98fabfd4fb49
gnittingCodes <shing.ng9495@gmail.com>
2024-08-24 21:26:21 +1000

    update landing aligned

--- the-bugger/src/pages/Landing.js
+++ the-bugger/src/pages/Landing.js
@@ -5,22 +5,23 @@ const LandingPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-[#faf4ef]">
       <main className="flex flex-col items-center mt-10">
-        <h1 className="text-6xl font-semibold mb-6 text-[#5f5f5f]">The Bugger</h1>
-        <div className="flex space-x-4 items-center">
+      <h1 className="text-8xl font-semibold mb-16 text-[#5f5f5f]">The Bugger</h1>
+        <div className="w-80 flex items-center space-x-4">
           <input
             type="text"
             placeholder="Code"
-            className="px-4 py-2 border-none rounded-full bg-[#a16667] text-white text-center placeholder-white placeholder-opacity-50 focus:outline-none"
+            className="flex-grow px-4 py-2 border-none rounded-full bg-[#a16667] text-white text-center placeholder-white placeholder-opacity-50 focus:outline-none"
           />
-          <button className="bg-[#d8b494] text-white px-6 py-2 rounded-full">
+          <button className="w-24 bg-[#d8b494] text-white px-4 py-2 rounded-full">
             Join
           </button>
         </div>
-        <button className="mt-6 bg-[#d8b494] text-white px-8 py-3 rounded-full">
+        <button className="mt-4 w-80 bg-[#d8b494] text-white px-4 py-2 rounded-full">
           Create your own room
         </button>
       </main>
     </div>
   );
 };
+
 export default LandingPage;
