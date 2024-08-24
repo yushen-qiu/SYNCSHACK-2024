68298c2c870f53928b68c04477d50daf90189d83
Alan Xie <alanxie123@gmail.com>
2024-08-24 20:42:22 +1000

    something's showing

---
+++ the-bugger/src/pages/GamePage.js
@@ -0,0 +1,13 @@
+// src/pages/GamePage.js
+import React from 'react';
+
+function GamePage() {
+  return (
+    <div className="game-page p-4 bg-gray-100 min-h-screen">
+      <h1 className="text-3xl font-bold text-center">Welcome to the Game Page</h1>
+      {/* Add your game logic and components here */}
+    </div>
+  );
+}
+
+export default GamePage;
